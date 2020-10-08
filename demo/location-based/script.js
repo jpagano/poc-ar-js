a22497c681f01834275529264c4d98494627d36c
J.Pagano <jordi@cenics.cat>
2020-10-08 18:25:55 +0200

    New example

--- demo/location-based/script.js
+++ demo/location-based/script.js
@@ -32,9 +32,9 @@ function renderPlaces(places) {
     let model = document.createElement('a-entity');
     model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
     model.setAttribute('gltf-model', './assets/plush_toy/scene.gltf');
-    model.setAttribute('rotation', '0 180 0');
+    model.setAttribute('rotation', '0 0 0');
     model.setAttribute('animation-mixer', '');
-    model.setAttribute('scale', '0.5 0.5 0.5');
+    model.setAttribute('scale', '1 1 1');
 
     model.addEventListener('loaded', () => {
       window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
